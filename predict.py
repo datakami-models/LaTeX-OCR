100f1bf2f0ec65e77053774cce67ed88ec473ebb
Mickey Beurskens <mickeybeurskens@gmail.com>
2023-11-06 11:41:40 +0000

    Load model in predict file and run inference with cog

--- predict.py
+++ predict.py
@@ -2,21 +2,28 @@
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
 from cog import BasePredictor, Input, Path
+from PIL import Image
+from pix2tex.cli import LatexOCR
 
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
-        # self.model = torch.load("./weights.pth")
+        self.model = LatexOCR()
 
     def predict(
         self,
-        image: Path = Input(description="Grayscale input image"),
-        scale: float = Input(
-            description="Factor to scale image by", ge=0, le=10, default=1.5
-        ),
-    ) -> Path:
+        image_path: Path = Input(description="Input image"),
+    ) -> str:
         """Run a single prediction on the model"""
-        # processed_input = preprocess(image)
-        # output = self.model(processed_image, scale)
-        # return postprocess(output)
+        image_data = load_image(str(image_path))
+        return self.model(image_data)
+
+
+def load_image(image_path: str) -> Image:
+    if image_path.startswith('http') or image_path.startswith('https'):
+        response = requests.get(image_path)
+        image = Image.open(BytesIO(response.content)).convert('RGB')
+    else:
+        image = Image.open(image_path).convert('RGB')
+    return image
